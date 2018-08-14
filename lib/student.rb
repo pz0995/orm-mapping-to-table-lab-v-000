dd8589ac78eb63ee8ea3565c526253e04d65eb7a
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-14 16:14:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,45 @@
+require 'pry'
+
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_accessor :name, :grade, :db
+  attr_reader :id
+
+    def initialize(name, grade, id=nil)
+      @name = name
+      @grade = grade
+      @id = id
+      @db = db
+    end
+
+    def self.create_table
+      sql = DB[:conn].execute('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade VARCHAR);')
+    end
+
+    def self.drop_table
+      sql = DB[:conn].execute('DROP TABLE IF EXISTS students;')
+    end
+
+    def save
+      sql = DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", name, grade)
+
+      value = DB[:conn].execute("SELECT id FROM students ORDER BY ID DESC LIMIT 1;").flatten
+      @id = value[0][0]
+      # INSERT INTO Student name, grade VALUES (?, ?)
+    # DB[:conn].execute(sql, self.name, self.album)
 end
+    def self.create(name:, grade:)
+
+  #     students = Student.new(id: id, name: name, grade: grade)
+  # id = students[0]
+  #     return students
+      # student.sql = DB[:conn].execute('SELECT * FROM students ORDER BY ID DESC LIMIT 1')
+
+students = DB[:conn].execute("SELECT * FROM students ORDER BY ID DESC LIMIT 1;").flatten
+students = hash.last
+# return
+
+      # return student.last
+    end
+
+  end
