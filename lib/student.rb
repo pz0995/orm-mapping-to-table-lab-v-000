9a667b0f3d997020eb494e047d868031b5b71dd9
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-14 16:26:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ end
   #     return students
       # student.sql = DB[:conn].execute('SELECT * FROM students ORDER BY ID DESC LIMIT 1')
 
-students = DB[:conn].execute("SELECT * FROM students ORDER BY ID DESC LIMIT 1;").flatten
+students = DB[:conn].execute("SELECT * FROM students ORDER BY ID DESC LIMIT 1;")
 return students.to_a.last
 # return
 
